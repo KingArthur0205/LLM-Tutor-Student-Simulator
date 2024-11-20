44ddf5b5c324671ca1470b090d6e39aeb40165b0
KingArthur0205 <gpan20020205@gmail.com>
2024-11-20 15:14:42 +0000

    Added README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-utils.py: Define the prompts and some utility functions.
+# STEM Tutor for Effective Problem-Solving
+The STEM Tutor for Effective Problem-Solving is an AI-powered educational tool designed to guide students through structured problem-solving in STEM subjects.
 
-student_simulator.py: Define the PhysicsTutor, PhysicsStudent, and LLMEvaluator(for LLMScore computation) classes.
+## Project Menu
+ - [src/main.py](https://github.com/KingArthur0205/LLM-Tutor-Student-Simulator/blob/main/src/main.py): Define the main workflow of the project
+ - [src/utils.py](https://github.com/KingArthur0205/LLM-Tutor-Student-Simulator/blob/main/src/utils.py): Contains prompt definitions and various utility functions to support the simulator.
+ - [src/physics_student.py](https://github.com/KingArthur0205/LLM-Tutor-Student-Simulator/blob/main/src/physics_student.py): Defines the student simulator based on Claude-3.5-Sonnet. It utilizes the StudentProfile class to simulate the student's behavior.
+ - [src/physics_tutor.py](https://github.com/KingArthur0205/LLM-Tutor-Student-Simulator/blob/main/src/physics_tutor.py): Defines the tutor simulator powered by GPT-4-Turbo.
+ - [src/gpt_evaluator.py](https://github.com/KingArthur0205/LLM-Tutor-Student-Simulator/blob/main/src/gpt_evaluator.py): Implements the GPT-4-Turbo-based LLMScore calculator, which evaluates and compares student and tutor responses against a predefined set of required materials.
--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ from physics_student import PhysicsStudentSimulator, profile_gen
 from gpt_evaluator import GPTEvaluator, generate_log_row
 from physics_tutor import PhysicsTutorSimulator
 
+def create_LLM_agents(profile):
+    claude_student_simulator = PhysicsStudentSimulator(os.environ.get("ANTHROPIC_API_KEY"), profile, utils.physics_problem)
+    gpt_tutor_simulator = PhysicsTutorSimulator(os.environ.get("OPENAI_API_KEY"))
+    gpt_evaluator = GPTEvaluator(os.environ.get("OPENAI_API_KEY"))
+    return claude_student_simulator, gpt_tutor_simulator, gpt_evaluator
+
 def main():
     # Load enviornment for LLM APIs
     _ = load_dotenv(find_dotenv())
@@ -14,9 +20,7 @@ def main():
     profile = profile_gen()
 
     # Create Student, Tutor, LLMScore evaluator
-    claude_student_simulator = PhysicsStudentSimulator(os.environ.get("ANTHROPIC_API_KEY"), profile, utils.physics_problem)
-    gpt_tutor_simulator = PhysicsTutorSimulator(os.environ.get("OPENAI_API_KEY"))
-    gpt_evaluator = GPTEvaluator(os.environ.get("OPENAI_API_KEY"))
+    claude_student_simulator, gpt_tutor_simulator, gpt_evaluator = create_LLM_agents(profile)
 
     student_response = "Can you help me with this question?"
     print(f"Student: {student_response}\n")
@@ -43,7 +47,7 @@ def main():
     except KeyboardInterrupt:
         print("\nProgram terminated by user")
 
-    student_response = claude_student_simulator.generate_response(tutor_question="Can you provide a concise summary of the key knowledge and insights you’ve gained so far to demonstrate your understanding and progress in this study?")
+    student_response = claude_student_simulator.generate_response(tutor_question="Can you provide a concise summary of the key knowledge and insights you've gained so far to demonstrate your understanding and progress in this study?")
     row = generate_log_row(profile,gpt_evaluator, gpt_tutor_simulator.get_conversation_history(), tutor_response, student_response, conversation_counter, tutor_response_len, student_response_len)
     utils.write_data(row) # Record the data into a csv file in the data directory
 
